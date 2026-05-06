7119a1c6e631bf42d41b38f3cabd3c34e1a9df8c
SebaS01010101 <s.sandoval18@ufromail.cl>
2026-05-06 08:53:19 -0400

    init

---
+++ src/test/java/com/EjercicioAyudantia/ISoft/ISoftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.EjercicioAyudantia.ISoft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ISoftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
