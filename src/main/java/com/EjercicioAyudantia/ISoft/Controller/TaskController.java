a5461c816e4ad1af04435893da92f71e577f40ec
Eduardo532 <112228065+Eduardo532@users.noreply.github.com>
2026-05-06 10:17:14 -0400

    feat: implementar endpoint GET /tasks con filtros por prioridad, titulo y fecha

--- src/main/java/com/EjercicioAyudantia/ISoft/Controller/TaskController.java
+++ src/main/java/com/EjercicioAyudantia/ISoft/Controller/TaskController.java
@@ -1,4 +1,40 @@
 package com.EjercicioAyudantia.ISoft.Controller;
 
+import com.EjercicioAyudantia.ISoft.Model.Task;
+import com.EjercicioAyudantia.ISoft.Service.TaskService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/tasks")
 public class TaskController {
+
+    private final TaskService taskService;
+
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Task>> getTAsks(
+            @RequestParam(required = false) String priority,
+            @RequestParam(required = false) String title,
+            @RequestParam(required = false) String limtDate
+    ){
+        List<Task> tasks = taskService.getTasks(priority, title, limtDate);
+        return ResponseEntity.ok(tasks);
+    }
+
+
+
+
+
+
+
 }
--- src/main/java/com/EjercicioAyudantia/ISoft/Service/TaskService.java
+++ src/main/java/com/EjercicioAyudantia/ISoft/Service/TaskService.java
@@ -1,4 +1,22 @@
 package com.EjercicioAyudantia.ISoft.Service;
 
+import com.EjercicioAyudantia.ISoft.Model.Task;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
 public class TaskService {
+
+    private final List<Task> tasks = new ArrayList<>();
+
+    public List<Task> getTasks(String priority, String title, String limitDate) {
+        return tasks.stream()
+                .filter(task -> priority == null || task.getPriority().equalsIgnoreCase(priority))
+                .filter(task -> title == null || task.getTitle().toLowerCase().contains(title.toLowerCase()))
+                .filter(task -> limitDate == null || task.getLimitDate().equals(limitDate))
+                .collect(Collectors.toList());
+    }
 }
