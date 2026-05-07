a3a0ec309520b05807a8356334216dd22e4aeb6c
Emilia Toro <e.toro02@ufromail.cl>
2026-05-07 15:53:19 -0400

    feat: agregando endpoint POST /tasks

---
+++ src/main/java/com/EjercicioAyudantia/ISoft/Task.java
@@ -0,0 +1,26 @@
+package com.EjercicioAyudantia.ISoft;
+
+public class Task {
+    private static Long counter = 1L;
+    private Long id;
+    private String titulo;
+    private String prioridad;
+    private String fechaLimite;
+    private boolean completada;
+
+    public Task(String titulo, String prioridad, String fechaLimite) {
+        this.id = counter++;
+        this.titulo = titulo;
+        this.prioridad = prioridad;
+        this.fechaLimite = fechaLimite;
+    }
+
+    public Long getId() { return id; }
+    public String getTitulo() { return titulo; }
+    public String getPrioridad() { return prioridad; }
+    public String getFechaLimite() { return fechaLimite; }
+    public boolean isCompletada() { return completada; }
+    public void setCompletada(boolean completada) { this.completada = completada; 
+
+    }
+}
